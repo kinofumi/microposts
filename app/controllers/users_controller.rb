9be4b7f98e267a415601570d3d9d5909f8a2fe9e
Kinoshita Fumichika <fumichika@under-the-tree.jp>
2016-11-30 07:23:34 +0000

    ユーザー情報の編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -16,8 +18,24 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-    
+
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "ユーザー情報を更新しました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
